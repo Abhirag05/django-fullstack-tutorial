0e2e012c0b29878622e88c5d40d4a00bd26a84e8
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-06-19 14:07:44 +0530

    feat:created the initial migration with custom user model

--- config/settings.py
+++ config/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
         "PORT": config("DB_PORT"),
     }
 }
-
+AUTH_USER_MODEL = "users.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import include,path
 
 urlpatterns = [
-    path('accounts/',include('accounts.urls')),
     path('admin/', admin.site.urls),
     
 ]
