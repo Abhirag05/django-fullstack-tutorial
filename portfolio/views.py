cd1b764f4c6fe871bbf1ffe5df0b5d0b3014c153
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-07-30 20:34:30 +0530

    feat: update home view to pass project data to template

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
+from .models import Project
 # Create your views here.
 def home(request):
-    return render(request, 'portfolio/home.html')
+    context = {
+        'projects': Project.objects.all()
+    }
+    return render(request, 'portfolio/home.html',context)
 
 def about(request):
     return render(request, 'portfolio/about.html')
