1fbfe278c6e7fb71ead73ad7d7a0be10b9e25b9a
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-07-30 20:29:49 +0530

    feat: restructure portfolio templates and update view paths

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'portfolio/home.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'portfolio/about.html')
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'portfolio/contact.html')
