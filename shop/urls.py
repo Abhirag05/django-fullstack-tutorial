12f39e383eeebd4961d8f55a8091658d5067accd
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-08-01 22:25:07 +0530

    feat: remove shop app and its related files

--- shop/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns=[
-    path('shop-home/', views.home, name="shop-home"),
-    path('shop-about/', views.shop, name="shop-about"),
-    path('shop-about/<int:id>/',views.product_details,name="product-detail"),
-]
