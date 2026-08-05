7f9c2b94512d7c9f9e7fb9ab0dc93ad69a1335d7
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-08-05 20:24:02 +0530

    feat: implement Todo model, views, and templates for todo list functionality

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import Todo
 
 # Create your views here.
 
 def todo_list(request):
-    return render(request,'todo/todo_list.html')
+    todo_data=Todo.objects.all()
+    return render(request,'todo/todo_list.html', {'todo_data': todo_data})
 
 def todo_create(request):
     return render(request,'todo/todo_create.html')
