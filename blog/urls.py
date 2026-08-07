d477406edc6b1141cd771db38348ae88f044de12
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-08-07 21:14:58 +0530

    feat: implement Blog model, views, forms, and create blog post functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path,re_path
 from . import views
 
+app_name="blog"
+
 urlpatterns=[
     path('blog-home/', views.home, name="blog-home"),
     path('blog-about/', views.about, name="blog-about"),
     re_path(r'^blog-about/(?P<year>[0-9]{4})/$',views.article_by_year,name="article_by_year"),
     path('blog-about/<int:year>/<str:month>/',views.article_details,name="article_details"),
-    path('article-filter/',views.article_filter,name="article_filter")
+    path('article-filter/',views.article_filter,name="article_filter"),
+    path('create-blog/',views.create_blog,name="create_blog")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from datetime import datetime
-
+from .forms import BlogForm
+from django.contrib import messages
+from .models import Blog 
 # Create your views here.
 def home(request):
     return render(request,'blogs/index.html')
@@ -33,4 +35,12 @@ def article_filter(request):
         }
     return render(request, 'blogs/article_filter.html', {'post_list': post_list})
 
-        
+def create_blog(request):
+    form=BlogForm()
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Blog created successfully!")
+            return redirect('blog:create_blog')
+    return render(request, 'blogs/create_blog.html', {'form': form})
