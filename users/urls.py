8a95f509fe584c519e55026c75b53dfbd97d09c5
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-08-10 11:37:03 +0530

    feat: add user authentication views and templates for registration, login, logout, and dashboard

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,9 @@ from django.urls import path
 urlpatterns = [
     path('',views.home,name="home"),
     path('messages/',views.show_messages,name="messages"),
+    path('register/',views.register_view,name="register_view"),
+    path('login/',views.login_view,name="login_view"),
+    path('logout/',views.logout_view,name="logout_view"),
+    path('dashboard/',views.dashboard,name="dashboard"),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from .forms import UserRegisterForm
 from django.contrib import messages
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 class User:
@@ -31,3 +34,40 @@ def show_messages(request):
     messages.warning(request, 'This is a warning message.')
     messages.error(request, 'This is an error message.')
     return render(request, 'users/messages.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            login(request,user)
+            messages.success(request, 'Your account has been created! You are now logged in.')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, 'You have been logged in.')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'You have been logged out.')
+    return redirect('login_view')
+
+@login_required
+def dashboard(request):
+    return render(request, 'users/dashboard.html')
