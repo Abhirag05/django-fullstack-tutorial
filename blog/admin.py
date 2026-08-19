acdc6cd564d663ad4fefd7a4d38fd2ee2442def1
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-08-19 12:34:15 +0530

    feat: add cache clearing functionality in admin panel for Blog model

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import Blog
+from django.core.cache import cache
 
-# Register your models here.
+#function for clearing the cache from the admin panel
+def clear_cache(modeladmin, request, queryset):
+    cache.clear()
+    modeladmin.message_user(request, "Cache cleared successfully.")
+
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'created_at', 'updated_at')
     search_fields = ('title', 'content', 'category')
     list_filter = ('category', 'created_at', 'updated_at')
+    actions=['clear_cache']
