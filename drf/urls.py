bef6f2bd40f3b3a7702bcf70e182f2d5509d3499
Abhirag.S.V <abhiragsv2005@gmail.com>
2026-08-21 12:29:35 +0530

    feat: add student management API with list and add functionality

--- drf/urls.py
+++ drf/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import student_list
+from .views import student_list,add_student
 
 urlpatterns = [
-    path('get-students/', student_list, name='student-list'),
+    path('students/', student_list, name='student-list'),
+    path('students/add/', add_student, name='add-student')
+
 ]
--- drf/views.py
+++ drf/views.py
@@ -3,9 +3,20 @@ from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from drf_spectacular.utils import extend_schema 
 
 @api_view(['GET'])
 def student_list(request):
     students = Student.objects.all()
     serializer = StudentSerializer(students, many=True)
     return Response(serializer.data)
+
+@extend_schema(request=StudentSerializer, responses=StudentSerializer)
+@api_view(['POST'])
+def add_student(request):
+    serializer = StudentSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
